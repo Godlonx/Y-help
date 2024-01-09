d2645c46946c57d184fd02745f825ba9a60509ac
Godlonx <godlonx44@gmail.com>
2024-01-09 09:52:55 +0100

    :white_check_mark: | working path for all pages

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home_projects.html')
 
-@app.route("/projet")
+@app.route("/projets")
 def projects():
-    return render_template('projects.html')
+    return render_template('home_projects.html')
 
 @app.route("/projet/<id>")
 def projet(id):
     return render_template('project.html')
+
+@app.route("/freelancers")
+def freelancers():
+    return render_template('home_freelancers.html')
+
+@app.route("/freelancer/<id>")
+def freelancer(id):
+    return render_template('freelancer.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
